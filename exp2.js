8765714fad7e833daa34db01113abd356ad162d6
shreyashchougule08 <shreyashchougule26@gmail.com>
2026-04-29 00:03:22 -0700

    Add files via upload

---
+++ exp2.js
@@ -0,0 +1,28 @@
+// 1 declare one array and object and string
+// 2 find largest number in array
+// 3 find reverse number in string 
+// 4 check palindrome in string
+// 5 write code fro phibonacci series
+// 6 count vowel in string 
+// 7 remove duplicate in array
+// 8 function !to find even odd
+// 9 code for sum of Array
+
+
+// activity 01
+let Arr1 = [1,3,5,7,9];
+console.log(Arr1);
+
+let obj1 = {
+  age: 21
+}
+console.log(obj1);
+
+let name="shreyash"
+console.log(name);
+
+// activity 02
+let arr2=[34,67,23,89,12,90,45];
+console.log(Math.max(...arr2));
+
+// activity 03
---
+++ exp3.js
@@ -0,0 +1,12 @@
+// diffn bet arrow function and simple function 
+// this  key word - why we use use arrow function and simple function if not why 
+// write code for  arrow function with 2 example 
+// write code for switch case in javascript with example
+// how to use truthy and falsy values in javascript with example
+// how to use turnary operator in javascript with example
+// write code for loops use in array  (do while.while ,for loop,for each loop) with example
+// diffn bet forin and for of loop with example
+// how to use filter and map function in javascript with example
+// how to use reduce function in javascript 
+// how to use reduce method in javascript
+
